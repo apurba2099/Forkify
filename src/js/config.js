747581f853035825219560ab59b2da817be193ff
Apurba Dutta <apurbadutta2099@gmail.com>
2025-05-30 15:03:30 +0530

    adding JS Final Project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '32ebb452-18e0-479a-a1de-f1b2fa6d9530';
+export const MODAL_CLOSE_SEC = 2.5;
